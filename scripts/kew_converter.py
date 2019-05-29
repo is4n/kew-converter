b6aa2d997cdb02c1a330575a3fa1d595765b1438
qazxswedcvfrtgbnhyujmkiolp <2b377c205d@gmail.com>
2019-05-29 12:07:52 -0500

    main app working

--- scripts/kew_converter.py
+++ scripts/kew_converter.py
@@ -1,15 +1,36 @@
 #TODO main script that will be run by the user
 
 import sys
+import os
+import conf
+import gets_swf
 
 if __name__ == "__main__":
-	arguments = sys.argv
-
-	if (len(arguments) = 0):
-		print ("usage: kew-converter <KEW_url>"
+	arguments = sys.argv[1:]
 	
-	else if (len(arguments) = 1):
-		kewPath = arguments[0]
+	# print usage if no input
+	if (len(arguments) < 2):
+		print ("usage: kew-converter <KEW_url> <output> [-z]")
+		sys.exit(0)
+	
+	# checks validity of arguments
+	
+	# check that output path exists
+	if (not os.path.exists(arguments[1])): 
+		print ("Could not find path " + arguments[1])
+		sys.exit(0)
 
-		gets_swf.fetch_all(kewPath, conf.get_prop("slide_dump_dir"))
-		gets_swf.extract_audio_all(conf.get_prop("slide_dump_dir"))
+	# set output_dir
+	conf.set_prop("output_dir", arguments[1])
+
+	kewUrl = arguments[0]
+	
+	print("STEP 1: Acquiring KEW slides from server")
+	gets_swf.fetch_all(kewUrl, conf.get_prop("output_dir"))
+	print("STEP 2: Fetching linked text")
+	gets_swf.get_linked_text_all(conf.get_prop("output_dir"))
+	print("STEP 3: Extracting audio")
+	gets_swf.extract_audio_all(conf.get_prop("output_dir"))
+	print("STEP 4: Extracting images")
+	gets_swf.extract_images_all(conf.get_prop("output_dir"))
+
